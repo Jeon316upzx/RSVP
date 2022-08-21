7955c942234bef7351e6bbf32183c5f12e5eb2d1
Jeon316upzx <mamasbwoyjeon94@gmail.com>
2022-08-21 13:11:43 +0100

    sc: deploy to testnet and test

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks:{
+    rinkeby: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  }
 };
 
 export default config;
